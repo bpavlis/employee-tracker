bc87535bc1bc51984f8e3b76756b8171a259524c
bpavlis <benpavlis@gmail.com>
2023-11-12 16:04:30 -0600

    finished up server.js function work, also wrote if statements

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const inquirer = require('inquirer');
 const express = require("express");
 const path = require("path");
 const app = express();
-const routes = require("./routes");
-const sequelize = require("./config/connection")
+// const routes = require("./routes");
+// const sequelize = require("./config/connection")
 const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 
@@ -35,58 +35,61 @@ function choices() {
     }
   ]) .then(function(results) {
     //if else statements here
+    if (results.optionPicked === "View departments") {
+      viewDepartments()
+    } else if (results.optionPicked === "View roles") {
+        viewRoles()
+    } else if (results.optionPicked === "View employees") {
+        viewEmployees()
+    } else if (results.optionPicked === "Add department") {
+        addDepartments()
+    } else if (results.optionPicked === "Add role") {
+        addRoles()
+    } else if (results.optionPicked === "Add employee") {
+        addEmployees()
+    } else if (results.optionPicked === "Update employee's role") {
+        updateEmployeeRole()
+    } else {
+      db.end();
+    }
   })
 }
 
 
 
 function viewDepartments() {
-  app.get('/api/departments', (req, res) => {
     const view = `SELECT * FROM department`;
 
-    db.query(view, (err, rows) => {
+    db.query(view, (err, res) => {
+      console.table(res)
       if (err) {
         res.status(500).json({ error: err.message });
          return;
       }
-      res.json({
-        message: 'success',
-        data: rows
-      });
     });
-  });
 }
-function viewRoles() {
-  app.get('/api/roles', (req, res) => {
-    const view = `SELECT * FROM roles`;
 
-    db.query(view, (err, rows) => {
+function viewRoles() {
+    const view = `SELECT * FROM role`;
+
+    db.query(view, (err, res) => {
+      console.table(res)
       if (err) {
         res.status(500).json({ error: err.message });
          return;
       }
-      res.json({
-        message: 'success',
-        data: rows
-      });
     });
-  });
 }
 function viewEmployees() {
-  app.get('/api/employees', (req, res) => {
-    const view = `SELECT * FROM employees`;
-
-    db.query(view, (err, rows) => {
+    const view = `SELECT * FROM employee`;
+    db.query(view, (err, res) => {
+      
       if (err) {
         res.status(500).json({ error: err.message });
          return;
       }
-      res.json({
-        message: 'success',
-        data: rows
-      });
+      return console.table(res)
     });
-  });
 }
 
 
@@ -96,119 +99,117 @@ function addDepartments() {
     {
       type: "input",
       message: "Enter the name of the new department:",
-      name: "name",
+      name: "newName",
     }
-  ]).then ((answers) => {
-    app.post('/api/new-department', ({ body }, res) => {
-      const insert = `INSERT INTO department (name)
-        VALUES (?)`;
-      const params = [body.name];
-      
-      db.query(insert, params, (err, result) => {
+  ]).then ((result) => { 
+      db.query(`INSERT INTO department (name) VALUES (?)`, [result.newName], (err, res) => {
+        console.table(viewDepartments())
         if (err) {
           res.status(400).json({ error: err.message });
           return;
-        }
-        res.json({
-          message: 'success',
-          data: body
-        });
+        }        
       });
     });
-  })
-    
-    // function(results) {
-    // db.query(`INSERT INTO department (name)
-    // VALUES (?)`, [results.newName], function(err, res){
-    //   if (err) {
-    //     res.status(400).json({ error: err.message });
-    //     return;
-    //   }
-    // })
-
-
-
 }
+
 function addRoles() {
   inquirer.prompt([
     {
       type: "input",
       message: "Enter the title of the new role:",
-      name: "title",
+      name: "newTitle",
     },
     {
       type: "number",
       message: "Enter the salary of the new role:",
-      name: "salary",
+      name: "newSalary",
     },
     {
       type: "number",
       message: "Enter the department_id of the new role:",
-      name: "department_id",
+      name: "newDepartment_id",
     }
-  ])
-
-  app.post('/api/new-role', ({ body }, res) => {
-    const insert = `INSERT INTO role (title, salary, department_id)
-      VALUES (?, ?, ?)`;
-    const params = [body.title, body.salary, body.department_id];
-    
-    db.query(insert, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: body
+  ]).then ((result) => {
+      
+      db.query(`INSERT INTO role (title, salary, department_id)
+      VALUES (?, ?, ?)`, [result.newTitle, result.newSalary, result.newDepartment_id], (err, res) => {
+        console.table(viewRoles())
+        if (err) {
+          res.status(400).json({ error: err.message });
+          return;
+        }
       });
     });
-  });
 }
+
 function addEmployees() {
   inquirer.prompt([
     {
       type: "input",
       message: "Enter the first name of the new employee:",
-      name: "first_name",
+      name: "newFirst_name",
     },
     {
       type: "input",
       message: "Enter the last name of the new employee:",
-      name: "last_name",
+      name: "newLast_name",
     },
     {
       type: "number",
       message: "Enter the role_id of the new employee:",
-      name: "role_id",
+      name: "newRole_id",
     },
     {
       type: "number",
-      message: "Enter the manager_id of the new employee (if any):",
-      name: "manager_id",
+      message: "Enter the manager_id of the new employee (enter null if none):",
+      name: "newManager_id",
     }
-  ])
-
-  app.post('/api/new-employee', ({ body }, res) => {
-    const insert = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-      VALUES (?, ?, ?, ?)`;
-    const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
-    
-    db.query(insert, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: body
+  ]).then ((result) => {
+      
+    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+      VALUES (?, ?, ?, ?)`, [result.newFirst_name, result.newLast_name, result.newRole_id, result.newManager_id], (err, res) => {
+        console.table(viewRoles())
+        if (err) {
+          res.status(400).json({ error: err.message });
+          return;
+        }
       });
     });
-  });
 }
 
 
 
 function updateEmployeeRole() {
-  
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter the id of the employee to be updated:",
+      name: "updatedEmployee"
+    },
+    {
+      type: "input",
+      message: "Enter the change in role_id:",
+      name: "updatedRole"
+    }
+  ]).then ((result) => {
+      // const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+      // const params = [req.body.review, req.params.id];
+    
+      db.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [result.updatedRole, result.updatedEmployee], (err, res) => {
+        console.table(viewEmployees())
+        if (err) {
+          res.status(400).json({ error: err.message });
+        } 
+      });
+    });
 }
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+choices();
